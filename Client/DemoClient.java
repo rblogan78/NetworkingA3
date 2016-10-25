4aa758d9dd237e9c943038d3d0dbc272f6ae3015
Rob Logan <roblogan120@gmail.com>
2016-10-25 21:33:00 +1100

    created skeleton of CORBA interractions

--- Client/DemoClient.java
+++ Client/DemoClient.java
@@ -5,12 +5,24 @@ import org.omg.CORBA.*;
 
 public class DemoClient
 {
+    private static String random = "*";
     public static void main(String[] args)
     {
+        
         try
         {
             ORB orb = ORB.init(args, null);
 			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            
+            String name = "Demo";
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            demo demoRef = demoHelper.narrow(ncRef.resolve_str(name));
+            
+            for(int i=1; i<6;i++){
+                System.out.println(random);
+            }
+            
+            random = demoRef.getRandom();
             
         }catch(Exception e){
             e.printStackTrace();
--- Server/DemoServer.java
+++ Server/DemoServer.java
@@ -5,6 +5,7 @@ import org.omg.CORBA.*;
 import org.omg.PortableServer.*;
 import org.omg.PortableServer.POA;
 import java.util.Properties;
+import java.util.Random;
 
 public class DemoServer
 {
@@ -15,9 +16,44 @@ public class DemoServer
             ORB orb = ORB.init(args, null);
 			POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
 			rootpoa.the_POAManager().activate();
+
+            demoImpl demo = new demoImpl();
+      		demo.setORB(orb);
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(demoImpl);
+      	    demo href = demoHelper.narrow(ref);
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            String name = "Demo";
+            NameComponent[] path = ncRef.to_name(name);
+            ncRef.rebind(path, href);
+
+            orb.run();
         
         }catch(Exception e){
             e.printStacktrace();
         }
+    }
+}
+
+class DemoImpl extends demoPOA{
+
+    private ORB orb;
+
+    public void setORB(ORB orb_val){
+        orb = orb_val;
+    }
+  
+    public String getRandom(){
+        Random r = new Random();
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String rand = alphabet.charAt(r.nextInt(alphabet.length()));
+        
+        return rand;
+    }
+  
+    public void shutdown(){
+        orb.shutdown(false);
     }
 }
