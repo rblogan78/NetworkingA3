c02b908839ad6ea062364ea218882d35bff40b06
Rob Logan <roblogan120@gmail.com>
2016-10-31 22:43:08 +1100

    undated Client class with new loop structure

--- Client/DemoClient.java
+++ Client/DemoClient.java
@@ -6,9 +6,9 @@ import org.omg.CORBA.*;
 public class DemoClient
 {
     private static String random = "*";
+    private static int counter = 1;
     public static void main(String[] args)
-    {
-        
+    { 
         try
         {
             ORB orb = ORB.init(args, null);
@@ -18,16 +18,22 @@ public class DemoClient
             NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
             DemoRandom demoRef = DemoRandomHelper.narrow(ncRef.resolve_str(name));
             
-            for(int i=1; i<6; i++){
-                System.out.println(i+".  Value is "+random);
+            while (counter<6){
+                long inc = 0;
+                System.out.println(counter+".  Value is "+random);
+                for(int j=1;j<200000;j++){
+                    for(int l=1;l<200000;l++){
+                        inc++;
+                    }
+                }
+                counter++;
             }
             
             random = demoRef.getRandom();
-            int j=6;
             System.out.println("Call to Server...");
-            while(j<11){
-                System.out.println(j+".  Value is "+random);
-                j++;
+            while(counter<11){
+                System.out.println(counter+".  Value is "+random);
+                counter++;
             }
             
         }catch(Exception e){
